cd6029bd197eec6f3511698a338a825dd9a7b94d
abolfazl <abolfazl.ahmadi@snapp.cab>
2022-06-29 15:56:00 +0430

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {Label} from 'snapp-ui-kit-new'
+
+
+function App() {
+  return (
+   <Label />
+  );
+}
+
+export default App;
